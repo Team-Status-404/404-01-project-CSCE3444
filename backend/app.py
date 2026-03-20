19c1a2adb2f3fb40f45695d15d8e9b65d5516519
oladipupo-david-gideon <oladipupo.david.gideon@gmail.com>
2026-03-20 15:45:14 -0500

    updated app.py and created a test_db.py file to test it.

--- backend/app.py
+++ backend/app.py
@@ -11,11 +11,7 @@ from sentiment import analyze_stock_hype
 load_dotenv()
 
 app = Flask(__name__)
-<<<<<<< HEAD
 CORS(app) 
-=======
-CORS(app)
->>>>>>> origin/timilsenayasas-patch-1
 
 @app.route('/')
 def home():
@@ -24,7 +20,6 @@ def home():
 @app.route('/api/stock/<ticker>', methods=['GET'])
 def get_stock_data(ticker):
     """
-<<<<<<< HEAD
     Fetch stock price, name, and market cap using yfinance.
     """
     try:
@@ -33,11 +28,6 @@ def get_stock_data(ticker):
         info = stock.info
         
         # 2. Grab the most accurate recent price using the download method
-=======
-    Fetch stock price using yfinance download
-    """
-    try:
->>>>>>> origin/timilsenayasas-patch-1
         data = yf.download(
             ticker.upper(),
             period="5d",
@@ -51,18 +41,12 @@ def get_stock_data(ticker):
 
         current_price = float(data["Close"].dropna().iloc[-1])
 
-<<<<<<< HEAD
         # 3. Return a unified JSON object
         return jsonify({
             "ticker": ticker.upper(),
             "name": info.get("longName", "Unknown"),
             "current_price": round(current_price, 2),
             "marketCap": info.get("marketCap", "N/A")
-=======
-        return jsonify({
-            "ticker": ticker.upper(),
-            "current_price": round(current_price, 2)
->>>>>>> origin/timilsenayasas-patch-1
         }), 200
 
     except Exception as e:
@@ -80,8 +64,5 @@ def get_stock_sentiment(ticker):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     access_db_tables()
-=======
->>>>>>> origin/timilsenayasas-patch-1
     app.run(debug=True, port=5000)
